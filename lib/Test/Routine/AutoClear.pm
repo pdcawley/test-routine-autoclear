66170ae14cfb49cbc30220606bc460e27a930477
Piers Cawley <pdcawley@bofh.org.uk>
2012-07-18 10:52:32 -0700

    Added an autoclear trait
    If an attribute has the AutoClear trait, then we clear it after every
    call to run_test

--- lib/Test/Routine/AutoClear.pm
+++ lib/Test/Routine/AutoClear.pm
@@ -1,7 +1,21 @@
-use strict;
-use warnings;
-package Test::Routine::AutoClear
-use Moose;
+package Test::Routine::Meta::Attribute::Trait::AutoClear;
+use Moose::Role;
 
-__PACKAGE__->meta->make_immutable;
+package Moose::Meta::Attribute::Custom::Trait::AutoClear;
+sub register_implementation {
+    'Test::Routine::Meta::Attribute::Trait::AutoClear';
+}
+
+package Test::Routine::AutoClear;
+use Moose::Role;
+
+after run_test => sub {
+    my $self = shift;
+
+    $_->clear_value($self) foreach grep {
+        $_->does( 'Test::Routine::Meta::Attribute::Trait::AutoClear' )
+    } $self->meta->get_all_attributes;
+};
+
 1;
+# ABSTRACT: Enables autoclearing attrs in Test::Routines
