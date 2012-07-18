3cfac24098e31810ff6be65c96a048bf306a2f46
Piers Cawley <pdcawley@bofh.org.uk>
2012-07-18 14:37:50 -0700

    Whee! Documentation!
    Bad documentation, but documentation all the same.

--- lib/Test/Routine/AutoClear.pm
+++ lib/Test/Routine/AutoClear.pm
@@ -31,3 +31,48 @@ sub has {
     );
 }
 1;
+__END__
+
+=head1 SYNOPSIS
+
+    use Test::Routine::AutoClear;
+    use Test::More;
+    use File::Tempdir;
+
+    has _tempdir => (
+        is        => 'ro',
+        isa       => 'Int',
+        builder   => '_build_tempdir',
+        lazy      => 1,
+        autoclear => 1,
+        handles   => {
+            tempdir => 'name',
+        },
+    );
+
+    sub _build_tempdir {
+        File::Tempdir->new();
+    }
+
+And now all the tests that use a tempdir in your test routine will get a
+fresh Tempdir
+
+=head1 DESCRIPTION
+
+When I'm writing tests with L<Test::Routine> I find myself writing code like
+this all the time:
+
+    has counter => (
+        is      => ro,
+        lazy    => 1,
+        default => 0
+        lazy    => 1,
+        clearer => 'reset_counter',
+    );
+
+    
+
+=head1 SEE ALSO
+
+=for :list
+* L<Test::Routine>
