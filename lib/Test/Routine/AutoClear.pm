4e681b876f823820cea1910039c73f138ed2af67
Piers Cawley <pdcawley@bofh.org.uk>
2013-04-13 13:47:57 +0100

    Documentation is a good thing
    Even the kind of documentation I write is better than no documentation
    at all. Documentated the new reset logic.

--- lib/Test/Routine/AutoClear.pm
+++ lib/Test/Routine/AutoClear.pm
@@ -78,6 +78,40 @@ this all the time:
 And after about the first time, I got bored of doing this. So I started to fix
 it, and here's my first cut.
 
+L<Test::Routine::AutoClear> addresses this by adding a new C<autoclear> key to
+the C<has> arguments. If you set C<autoclear> to a true value on an attribute
+then, after each test is run, all the autoclearing attributes will be reset.
+
+=head2 Clearing logic
+
+Consider the following Test::Routine:
+
+    use Test::More;
+    use Test::Routine::AutoClear;
+    use Test::Routine::Util;
+
+    has some_attrib => (
+        is        => 'ro',
+        default   => 10,
+        lazy      => 1,
+        autoclear => 1,
+        clearer   => 'reset_attrib',
+    );
+
+    test "This should be invariant" => sub {
+        my $self = shift;
+        my $attrib = $self->attrib;
+        $self->reset_attrib;
+        is $self->attrib, $attrib;
+    };
+
+    run_me "Test defaults";
+    run_me "Test initialising", { attrib => 20 };
+    done_testing;
+
+It seems to me that, in a perfect world at least, that test should pass. Which
+it does. Huzzah!
+
 =head1 BUGS
 
 Lots. Including, but not limited to:
@@ -96,10 +130,8 @@ this role then you'll end up clearing your attributes lots of times.
 
 =item *
 
-I think it's reasonable to expect that resetting an attribute that
-didn't get set via a builder should reset the value to the initial
-value that was set via the instantiation params. Or maybe
-C<< autoclear => 1 >> should imply C<< init_arg => undef >>.
+Resetting to the initializing value only works for non reference values. Need
+some way of passing in a builder for hashrefs etc.
 
 =back
 
